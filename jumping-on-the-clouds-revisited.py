b5f43d5b440b6489d268148266b080502335b5e7
Aayush Gupta <aayushgupta.nitk11@gmail.com>
2016-08-18 00:38:05 +0530

    cloud jumping revisited

---
+++ jumping-on-the-clouds-revisited.py
@@ -0,0 +1,20 @@
+#!/mport sys
+# Problem URL: https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited
+
+n,k = raw_input().strip().split(' ')
+n,k = [int(n),int(k)]
+c = map(int,raw_input().strip().split(' '))
+E = 99
+if c[(k)%n] == 1:
+    E = 97
+i = (k)%n
+while i != 0:
+    E -= 1
+    if c[(i+k)%n] == 1:
+        E -= 2
+    i = (i+k)%n
+print Ebin/python
+
+import sys
+
+
