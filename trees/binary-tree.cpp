6d5c16d931d8dbbe940e9b1ff1f64f29b8191d2f
Aayush Gupta <aayushgupta.nitk11@gmail.com>
2016-11-07 23:07:07 +0530

    adding search and delete operation

--- trees/binary-tree.cpp
+++ trees/binary-tree.cpp
@@ -70,6 +70,51 @@ void postorder_traversal(node *root){
   return;
 }
 
+bool search(node *root, int key){
+  if (root == NULL)
+    return false;
+  if (root && root->value == key)
+    return true;
+  if (root && root->value > key)
+    return search(root->left, key);
+  if (root && root->value < key)
+    return search(root->right, key);
+  return false;
+}
+
+node *min_value_node(node *root){
+  node *current = root->right;
+  while (current->left)
+    current = current->left;
+  return current;
+}
+
+node *delete_node(node *root, int key){
+  if (root == NULL)
+    return root;
+  if (key < root->value){
+    root->left = delete_node(root->left, key);
+  } else if (key > root->value){
+    root->right = delete_node(root->right, key);
+  } else {
+    if (root->left == NULL){
+      node *temp = root->right;
+      free(root);
+      return temp;
+    } else if (root->right == NULL){
+      node *temp = root->left;
+      free(root);
+      return temp;
+    } else {
+      node *successor = min_value_node(root);
+      root->value = successor->value;
+      root->right = delete_node(root->right, successor->value);
+    }
+    return root;
+  }
+  return root;
+}
+
 int main(){
   node *root;
   root = NULL;
@@ -88,6 +133,25 @@ int main(){
   cout << endl << "===============================" << endl;
   cout << "Postorder traversal" << endl;
   postorder_traversal(root);
+  cout << endl << "===============================" << endl;
+  cout << "Search operations:" << endl;
+  cout << "Is 8 present? " << search(root, 8) << endl;
+  cout << "Is 12 present? " << search(root, 12) << endl;
+  cout << "Is 5 present? " << search(root, 5) << endl;
+  cout << "Is 25 present? " << search(root, 25) << endl;
+  cout << "Is 19 present? " << search(root, 19) << endl;
+  cout << "Delete NODES:" << endl;
+  root = delete_node(root, 18);
+  cout << "Inorder traversal: " << endl;
+  inorder_traversal(root);
+  cout << endl;
+  root = delete_node(root, 10);
+  cout << "Inorder traversal: " << endl;
+  inorder_traversal(root);
+  cout << endl;
+  root = delete_node(root, 25);
+  cout << "Inorder traversal: " << endl;
+  inorder_traversal(root);
   cout << endl;
   return 0;
 }
